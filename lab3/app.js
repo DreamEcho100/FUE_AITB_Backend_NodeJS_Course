74369f53ff09acda913f3e6df7bc62d1f776a34e
DreamEcho100 <maze6572198@gmail.com>
2022-08-06 11:46:06 +0200

    Updaing 'lab3'

--- lab3/app.js
+++ lab3/app.js
@@ -1,9 +1,27 @@
 // import os from 'os';
 // import path from 'path';
+import EventEmitter from 'events';
 import { isEvenOrOdd } from './utils/calc.js';
+import EVENTS from './utils/constants/events.js';
 
 // console.log(os.freemem());
 // console.log(os.totalmem());
 // console.log(path.dirname(path.resolve('.')));
 
-console.log(isEvenOrOdd(23));
+const numTest = 23;
+
+console.log(`${numTest} is ${isEvenOrOdd(23).toLowerCase()}.`);
+
+const event = new EventEmitter();
+
+event.on(EVENTS.ON_DATA_BASE_CONNECTION, () => console.log('connected'));
+
+event.on(EVENTS.ON_DATA_BASE_CONNECTION_LOST, () =>
+	console.log('connection closed')
+);
+
+event.emit(EVENTS.ON_DATA_BASE_CONNECTION);
+
+setTimeout(() => {
+	event.emit(EVENTS.ON_DATA_BASE_CONNECTION_LOST);
+}, 3000);
